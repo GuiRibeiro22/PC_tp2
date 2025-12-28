75a091d031d5cea28e575bfac676d2da0297d177
Xicodico <francisco.p.fernandes@ua.pt>
2025-12-28 19:24:31 +0000

    Updated C2_pClient_RL.py
    Agora kind of works, mas falta a parte dos rewards. And also something else que te vou perguntar Gui xD

--- C2_pClient_RL/C2_pClient_RL.py
+++ C2_pClient_RL/C2_pClient_RL.py
@@ -1,5 +1,6 @@
-from controller import Robot
+from controller import Robot,Receiver,Emitter
 import random
+import time
 
 # create the Robot instance.
 robot = Robot()
@@ -7,15 +8,13 @@ robot = Robot()
 # Get simulation step length.
 timeStep = int(robot.getBasicTimeStep())
 
-# Constants of the e-puck motors and distance sensors.
 cruiseVelocity = 5.0
 num_dist_sensors = 8
 
-# Get left and right wheel motors.
 leftMotor = robot.getDevice("left wheel motor")
 rightMotor = robot.getDevice("right wheel motor")
 
-# Get frontal distance sensors.
+
 dist_sensors = [robot.getDevice('ps' + str(x)) for x in range(num_dist_sensors)]  # distance sensors
 list(map((lambda s: s.enable(timeStep)), dist_sensors))  # Enable all distance sensors
 
@@ -33,23 +32,23 @@ camera.enable(timeStep)
 
 receiver = robot.getDevice("receiver")
 receiver.enable(timeStep)
-receiver.nextPacket()
 
 emitter = robot.getDevice("emitter")
 
-actions = ["Forward","Left","Right","MediumLeft","MediumRight"]
+ACTIONS = ["Forward", "Left", "Right", "MediumLeft", "MediumRight", "Back"]
 Q = {}
 
-alpha = 0.5
-gamma = 0.9
-epsilon = 0.1
+ALPHA = 0.5
+GAMMA = 0.9
+EPSILON = 0.1
 
-near_wall = 100
-near_front = 100
-near_side = 90
-danger_distance = 500
+NEAR_WALL = 100
+DANGER_DISTANCE = 300
 
 reset = False
+
+
+
 
 
 def perform_action(action):
@@ -68,13 +67,17 @@ def perform_action(action):
     elif action == "MediumRight":
         leftMotor.setVelocity(cruiseVelocity)
         rightMotor.setVelocity(-cruiseVelocity)
+    elif action == "Back":
+        leftMotor.setVelocity(-cruiseVelocity)
+        rightMotor.setVelocity(-cruiseVelocity)
+
 
 
 def get_state():
     Result = []
     dist_sensor_values = [g.getValue() for g in dist_sensors]
     for dist_sensor_value in dist_sensor_values:
-        if dist_sensor_value >= near_wall:
+        if dist_sensor_value >= NEAR_WALL:
             Result.append(0)
         else:
             Result.append(1)
@@ -82,85 +85,103 @@ def get_state():
     return tuple(Result)
     
 
-def avoid_walls():
+def Enable_Actions():
     dist_sensor_values = [g.getValue() for g in dist_sensors]
-    #print(dist_sensor_values)
+    EnabledActions = []
 
-    for dist_sensor_value in dist_sensor_values:
-        if dist_sensor_value > danger_distance:
-            print('back')
-            leftMotor.setVelocity (-cruiseVelocity)
-            rightMotor.setVelocity(-cruiseVelocity)
-            return
+    if dist_sensor_values[0] > DANGER_DISTANCE or dist_sensor_values[7] > DANGER_DISTANCE:
+        EnabledActions += ["Back"]
+        return EnabledActions
 
 
-    if dist_sensor_values[0] > near_front \
-    or dist_sensor_values[7] > near_front:
+    if dist_sensor_values[0] > NEAR_WALL \
+    or dist_sensor_values[7] > NEAR_WALL:
 
         if (dist_sensor_values[5] + dist_sensor_values[6] +dist_sensor_values[7]) < (dist_sensor_values[2] + dist_sensor_values[1]+dist_sensor_values[0]):
-            #print('Rotate Left')
-            leftMotor.setVelocity (-cruiseVelocity)
-            rightMotor.setVelocity(cruiseVelocity)
-        else:
-            #print('Rotate Right')
-            leftMotor.setVelocity (cruiseVelocity)
-            rightMotor.setVelocity(-cruiseVelocity)
+            EnabledActions += ["MediumLeft"]
 
-    elif dist_sensor_values[1]>near_side and dist_sensor_values[1]>dist_sensor_values[6]:
-        #print('turn left')        
-        leftMotor.setVelocity ( 0.1*cruiseVelocity)
-        rightMotor.setVelocity(cruiseVelocity)
-    elif dist_sensor_values[6]>near_side:
-        #print('turn right')
-        leftMotor.setVelocity (cruiseVelocity)
-        rightMotor.setVelocity( 0.1*cruiseVelocity)
+        if (dist_sensor_values[5] + dist_sensor_values[6] +dist_sensor_values[7]) > (dist_sensor_values[2] + dist_sensor_values[1]+dist_sensor_values[0]):
+            EnabledActions += ["MediumRight"]
+
+    if dist_sensor_values[1] > NEAR_WALL and dist_sensor_values[1] > dist_sensor_values[6]:
+        EnabledActions += ["Left"]  
+
+    if dist_sensor_values[6] > NEAR_WALL and dist_sensor_values[6] > dist_sensor_values[1]:
+        EnabledActions += ["Right"]
     else:
-        #print('go')
-        leftMotor.setVelocity(1.2*cruiseVelocity)
-        rightMotor.setVelocity(1.2*cruiseVelocity)
+        EnabledActions += ["Forward"]
 
-    return False
+    return EnabledActions
 
-def choose_action(state):
-    if random.random() < epsilon:
-        return random.choice(actions)
-    qs = [Q.get((state, a), 0.0) for a in actions]
+def choose_action(state,PossibleActions):
+    if random.random() < EPSILON:
+        return random.choice(PossibleActions)
+    
+    qs = [Q.get((state, a), 0.0) for a in PossibleActions]
     max_q = max(qs)
-    return random.choice([a for a, q in zip(actions, qs) if q == max_q])
+    return random.choice([a for a, q in zip(PossibleActions, qs) if q == max_q])
 
 
 def update_Q(state, action, reward, next_state):
     old = Q.get((state, action), 0.0)
-    next_max = max(Q.get((next_state, a), 0.0) for a in actions)
-    Q[(state, action)] = old + alpha * (reward + gamma * next_max - old)
+    next_possible = Enable_Actions()
+    next_max = max(Q.get((next_state, a), 0.0) for a in next_possible)
+    Q[(state, action)] = old + ALPHA * (reward + GAMMA * next_max - old)
+
+
 
 
 print("Started")
 
+current_state = get_state()
+old_reward = 0
+ACTION_STEPS = 5
+NUM_EPISODES = 20
+
 while robot.step(timeStep) != -1:
 
-    current_state = get_state()
-    current_action = choose_action(current_state)
-    if not avoid_walls():
-        perform_action(current_action)
+    for episode in range(NUM_EPISODES):
+        done = False
 
-    reset = False
+        while not done:
 
-    if receiver.getQueueLength() > 0 and not reset:
-        reward = receiver.getFloats()
-        print(reward)
+            current_state = get_state()
+            print("Current state: ",current_state)
 
-        next_state = get_state()
-        update_Q(current_state, current_action, reward, next_state)
 
-        emitter.send(b"Reset")
+            PossibleActions = Enable_Actions()
+            print("Possible actions: ", PossibleActions)
 
-        leftMotor.setVelocity(0)
-        rightMotor.setVelocity(0)
-
-        current_state = next_state
-        current_action = choose_action(current_state)
-        if not avoid_walls():
+            current_action = choose_action(current_state,PossibleActions)
             perform_action(current_action)
+            print("Action performed: ", current_action)
 
-        reset = True
+            # step simulation for the action to take effect
+            for _ in range(ACTION_STEPS):
+                robot.step(timeStep)
+
+            # Aqui recebemos um reward ------------------------
+            emitter.send("Action done")
+            #print("Emitter sent: Action done")
+            
+            if receiver.getQueueLength() > 0:
+                reward = receiver.getInts()[0]
+                final_reward = reward - old_reward
+                print(final_reward)
+
+            # ---------------------------------------------------
+
+                next_state = get_state()
+                update_Q(current_state,current_action,final_reward,next_state)
+
+                current_state = next_state
+                old_reward = reward
+
+
+                receiver.nextPacket()
+
+
+
+
+    
+    
